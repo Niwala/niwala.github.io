63f13b6523a14ae462bdd21ba09e8fe33bc57a05
Niwala <Niwala101@gmail.com>
2024-09-21 01:17:00 +0200

    Start parser

--- parser.js
+++ parser.js
@@ -1,17 +1,44 @@
-parse();
+Parse();
 
-function parse()
+function Parse()
 {
-	var contentContainer = document.getElementById("content-container");
-	
-	contentContainer.innerHTML = "test";
-	
-	fetch("sine.pack")
-	  .then((res) => res.text())
-	  .then((text) => {
-		  print(text);
-		// do something with "text"
-	   })
+	fetch("https://niwala.github.io/sine.json")
+		.then(response => response.json())
+		.then(jsonResponse => ReadJson(jsonResponse))     
 	  .catch((e) => console.error(e));
+	
+}
+
+function ReadJson(data)
+{
+	
+	//Variants
+	var switchField = document.getElementById("template-switch");
+	var templateSwitchButton = switchField.innerHTML;
+	var allSwitchButtons = "";
+	
+	for	(let i = 0; i < data.examples.length; i++)
+	{
+		var s = templateSwitchButton.replace(/button-id/g, "button" + i);
+		s = s.replace(/button-group/g, "group-0");
+		s = s.replace(/button-value/g, i == 0 ? "true" : "false");
+		s = s.replace(/template-button/g, data.examples[i].name);
+		allSwitchButtons += s;
+	}
+	
+	
+	switchField.innerHTML = allSwitchButtons;
+	
+	var templateElement = document.getElementById("template")
+	var template = templateElement.innerHTML;
+	// templateElement.style.display = 'none';
+	
+	template = template.replace(/template-title/g, data.name);
+	template = template.replace(/template-description/g, data.description);
+	
+
+	
+	var contentContainer = document.getElementById("content-container");
+	contentContainer.innerHTML = template + template + template + template;
 	
 }
