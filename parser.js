b8ee452944a555d20da0a4be27cb568154ca9f8c
Niwala <Niwala101@gmail.com>
2024-09-21 19:57:46 +0200

    Work on parser

--- parser.js
+++ parser.js
@@ -1,7 +1,16 @@
 Parse();
 
+//Templates
+var templateContainer;
+var templatePropertySlider;
+var templatePropertyColor;
+var templatePropertyCode;
+var templateSwitchButton;
+
 function Parse()
 {
+	LoadTemplates();
+	
 	fetch("https://niwala.github.io/sine.json")
 		.then(response => response.json())
 		.then(jsonResponse => ReadJson(jsonResponse))     
@@ -9,36 +18,78 @@ function Parse()
 	
 }
 
+function LoadTemplates()
+{
+	//Template container
+	container = document.getElementById("template")
+	templateContainer = container.innerHTML;
+	// container.style.display = 'none';
+	
+	//Property Slider
+	var propertySlider = document.getElementById("template-property-slider");
+	templatePropertySlider = propertySlider.innerHTML;
+	//propertySlider.style.display = 'none';
+	
+	//Property color
+	var propertyColor = document.getElementById("template-property-color");
+	templatePropertyColor = propertyColor.innerHTML;
+	//propertyColor.style.display = 'none';
+
+	//Code
+	var propertyCode = document.getElementById("template-code");
+	templatePropertyCode = propertyCode.innerHTML;
+	//propertyCode.style.display = 'none';
+
+	//Switch buttons
+	var switchField = document.getElementById("template-switch");
+	templateSwitchButton = switchField.innerHTML;
+	//switchField.style.display = 'none';
+}
+
 function ReadJson(data)
 {
-	
 	//Variants
-	var switchField = document.getElementById("template-switch");
-	var templateSwitchButton = switchField.innerHTML;
-	var allSwitchButtons = "";
+	// var allSwitchButtons = "";
 	
-	for	(let i = 0; i < data.examples.length; i++)
-	{
-		var s = templateSwitchButton.replace(/button-id/g, "button" + i);
-		s = s.replace(/button-group/g, "group-0");
-		s = s.replace(/button-value/g, i == 0 ? "true" : "false");
-		s = s.replace(/template-button/g, data.examples[i].name);
-		allSwitchButtons += s;
-	}
+	// for	(let i = 0; i < data.examples.length; i++)
+	// {
+		// var s = templateSwitchButton.replace(/button-id/g, "button" + i);
+		// s = s.replace(/button-group/g, "group-0");
+		// s = s.replace(/button-value/g, i == 0 ? "true" : "false");
+		// s = s.replace(/template-button/g, data.examples[i].name);
+		// allSwitchButtons += s;
+	// }
 	
 	
-	switchField.innerHTML = allSwitchButtons;
+	// switchField.innerHTML = allSwitchButtons;
 	
-	var templateElement = document.getElementById("template")
-	var template = templateElement.innerHTML;
-	// templateElement.style.display = 'none';
+	// //Build container
+	// var container = templateContainer;
+	// container = container.replace(/template-title/g, data.name);
+	// container = container.replace(/template-description/g, data.description);
 	
-	template = template.replace(/template-title/g, data.name);
-	template = template.replace(/template-description/g, data.description);
+	var content = templateContainer;
+	content = content.replace(/template-title/g, data.name);
+	content = content.replace(/template-description/g, data.description);
 	
+	
+	//Example
+	var exampleID = 0;
+	var example = data.examples[exampleID];
+	
+	content = content.replace(/example-name/g, example.name);
+	content = content.replace(/example-description/g, example.description);
+	
+	content = content.replace(/example-properties/g, BuildSlider("TestSlider", 2, 0, 5, 0.01, "TestSliderID"));
 
-	
 	var contentContainer = document.getElementById("content-container");
-	contentContainer.innerHTML = template + template + template + template;
+	contentContainer.innerHTML = content;
 	
+}
+
+function BuildSlider(name, value, minValue, maxValue, step, shaderID)
+{
+	return "<div class=\"slider-container\"><p>" + name + "</p>" + 
+	"<input type=\"range\" min=\"" + minValue + "\" max=\"" + maxValue + "\" value=\"" + value + "\" step=\"" + step + "\" class=\"slider\"/ id=\"slider-" + name +"\"></div>" +
+	"<p class=\"slider-value\"><span id=\"slider-field-" + name + "\">50</span></p>";
 }
