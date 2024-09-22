1cf992cdc8756d146cd5a54af68cc4dd6eac5c3b
Niwala <Niwala101@gmail.com>
2024-09-22 15:26:53 +0200

    Add shader function builder

---
+++ ShaderFunctionBuilder/Prompt.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShaderFunctionBuilder
+{
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox textBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+            Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+
+        public static DialogResult ShowSaveDialog(string text, string caption, string yesLabel, string noLabel)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text, Width = 400 };
+            prompt.Controls.Add(textLabel);
+            
+            Button confirmation = new Button() { Text = yesLabel, Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.Yes };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(confirmation);
+            
+            Button cancel = new Button() { Text = noLabel, Left = 240, Width = 100, Top = 70, DialogResult = DialogResult.No };
+            cancel.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(cancel);
+
+            prompt.AcceptButton = confirmation;
+            prompt.CancelButton = cancel;
+
+            return prompt.ShowDialog();
+        }
+    }
+}
---
+++ ShaderFunctionBuilder/ShaderFunction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShaderFunctionBuilder
+{
+    [Serializable]
+    public struct ShaderFunction
+    {
+        public string name;
+        public string tags;
+        public string description;
+
+        public ShaderExample[] examples;
+    }
+
+    [Serializable]
+    public struct ShaderExample
+    {
+        public string name;
+        public string description;
+        public string shader;
+        public string code;
+
+        public ShaderProperty[] properties;
+    }
+
+    [Serializable]
+    public struct ShaderProperty
+    {
+        public string name;
+        public string id;
+        public string type;
+        public string value;
+        public string min;
+        public string max;
+    }
+}
