22ebf4a187594170e10a605b23c27d94209c9708
Niwala <Niwala101@gmail.com>
2024-11-03 17:10:22 +0100

    Add all function preview properties

--- scripts/structs.js
+++ scripts/structs.js
@@ -1,13 +1,15 @@
 
 class FunctionPreview
 {
-
    constructor(json)
    {
       this.id = json.id;
       this.description = this.ValueFromRichText(json.properties.Description);
-
-
+      this.name = this.ValueFromTitle(json.properties.Name);
+      this.niceName = this.ValueFromRichText(json.properties.NiceName);
+      this.preview = this.ValueFromRichText(json.properties.Preview);
+      this.tags = this.ValueFromRichText(json.properties.Tags);
+      this.public = this.ValueFromCheckbox(json.properties.Public);
    }
 
    ValueFromRichText(property)
@@ -17,5 +19,15 @@ class FunctionPreview
       return property.rich_text[0].plain_text;
    }
 
+   ValueFromTitle(property)
+   {
+      if (property.title.length == 0)
+         return "";
+      return property.title[0].plain_text;
+   }
 
+   ValueFromCheckbox(property)
+   {
+      return property.checkbox;
+   }
 }
