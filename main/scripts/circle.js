6328ab3e1f5ed584189fff08f60830c2db1f3aa0
Niwala <Niwala101@gmail.com>
2025-06-07 23:38:29 +0200

    Rework presentation

--- main/scripts/circle.js
+++ main/scripts/circle.js
@@ -1,52 +1,197 @@
 
+const sectionOffset = 250;
+const sectionRange = 130;
+const globalOffsetAjustement = 100;
 
-const maxSize = 250;
-const minSize = 150;
-const shrinkDistance = 300;
+const titleLargeX = 0;
+const titleLargeY = 180;
+const titleSmallX = -60;
+const titleSmallY = -50;
+const titleLargeSize = 1.3;
+const titleSmallSize = 1;
+const speed = 6.0;
+
+let circle = null;
+let mainTitle = null;
+let sections = [];
+let sectionContents = [];
+let sectionTops = [];
+let offsets = [];
+
+let lastTime = performance.now();
+let currentScroll = 0.0;
+let smoothScroll = 0.0;
+let smoothGlobalOffset = 0.0;
+let circleOriginSize;
 
 InitializeCircle();
-SetCirclesize(maxSize);
+InitializeSections();
+OnScroll();
+Loop();
 
 function InitializeCircle()
 {
     //Load circle border
-    const circle = document.getElementById('circle');
+    circle = document.getElementById('circle');
     const img = new Image();
     img.alt = 'Circle Image';
+    circleOriginSize = parseFloat(circle.style.width);
 
-    //Load local image (from same directory, or use path if nested)
-    img.src = 'main/media/logo-border-sword.png';
+    // //Load local image (from same directory, or use path if nested)
+    // img.src = 'main/media/logo-border-sword.png';
 
-     //Append image once loaded
-     img.onload = () => {
-        circle.appendChild(img);
-      };
+    // //Append image once loaded
+    // img.onload = () => {
+    // circle.appendChild(img);
+    // };
+
+    //Load main title
+    mainTitle = document.getElementById('main-title');
 
     //Add scroll callback
-    window.addEventListener('scroll', () => 
-    {
-        //Scroll value
-        const scrollY = window.scrollY;
-    
-        //Clamp value between 0 and shrinkDistance
-        const t = Math.min(scrollY, shrinkDistance);
-    
-        //Lerp size
-        const size = maxSize - ((maxSize - minSize) * (t / shrinkDistance));
-    
-        //Apply size
-        SetCirclesize(size);
-
-        if (scrollY > minSize)
-        {
-            circle.style.top = (minSize-scrollY + 50.0) + 'px';
-        }
-    });
+    window.addEventListener('scroll', OnScroll);
 }
 
-
-function SetCirclesize(size)
+function InitializeSections()
 {
-    circle.style.width = size + 'px';
-    circle.style.height = size + 'px';
+    sections = Array.from(document.querySelectorAll(".section"));
+
+    offsets = new Array(sections.length + 1);
+    sectionTops = new Array(sections.length + 1);
+    sectionContents = new Array(sections.length);
+
+    for (let i = 0; i < sections.length; i++)
+    {
+        offsets[i] = 0.0;
+        sectionTops[i] = parseFloat(sections[i].style.top);
+        sectionContents[i] = sections[i].querySelector(".section-content");
+    }
+}
+
+function Lerp(a, b, t)
+{
+    return (a * (1.0 - t)) + (b * t);
+}
+
+function InverseLerp(a, b, t)
+{
+    return (t - a) / (b - a);
+}
+
+function Clamp(min, max, t)
+{
+    return Math.min(Math.max(t, min), max);
+}
+
+function Saturate(t)
+{
+    return Clamp(0.0, 1.0, t);
+}
+
+function Abs(t)
+{
+    return Math.abs(t);
+}
+
+function SmoothStep(a, b, t)
+{
+    t = Saturate(t);
+    t = t * t * (3.0 - 2.0 * t); 
+    return a + (b - a) * t;
+}
+
+function SCurve(t)
+{
+    return SmoothStep(0, 1, SmoothStep(0, 1, t));
+}
+
+function MoveToward(a, target, maxStep)
+{
+    let offset = target - a;
+    offset = Clamp(-maxStep, maxStep, offset);
+    return a + offset;
+}
+
+function OnScroll()
+{
+    //Scroll value
+    const scrollY = window.scrollY;
+
+    currentScroll = scrollY;
+}
+
+function Loop()
+{
+    //Loop stuff
+    const now = performance.now();
+    const deltaTime = (now - lastTime) / 1000.0;
+    lastTime = now;
+
+    //Circle
+    let circleSize = Math.max(offsets[sections.length - 1], 0) * 0.5;
+    circleSize = 1.0 - circleSize;
+    circle.style.width = (circleOriginSize * circleSize) + "px";
+    circle.style.height = (circleOriginSize * circleSize) + "px";
+    circle.style.borderWidth = Math.min(15, circleOriginSize * circleSize) + "px";
+
+    //Main title
+    let mtPosX = Lerp(titleSmallX, titleLargeX, circleSize);
+    let mtPosY = Lerp(titleSmallY, titleLargeY, circleSize);
+    mainTitle.style.translate = mtPosX + "px " + mtPosY + "px";
+    mainTitle.style.scale = Lerp(titleSmallSize, titleLargeSize, circleSize);
+
+
+    //Find best section
+    let nearestSection = 0;
+    let nearestDistance = 10000;
+    for (let i = 0; i < sections.length; i++)
+    {
+        let distance = Abs((sectionTops[i] - sectionOffset) - (currentScroll));
+        if (distance < nearestDistance)
+        {
+            nearestDistance = distance;
+            nearestSection = i;
+        }
+    }
+
+
+    //Smooth targeting
+    let targetScroll = sectionTops[nearestSection] - sectionOffset;
+    smoothScroll = Lerp(smoothScroll, targetScroll, deltaTime * 3.0);
+
+    let globalOffset = (smoothScroll - currentScroll) - globalOffsetAjustement;
+    smoothGlobalOffset = Lerp(smoothGlobalOffset, globalOffset, deltaTime * 3.0);
+
+    sectionTops[sections.length] = -350 - 15;
+    offsets[sections.length] = 0;
+
+    //Adapt sections position
+    for (let i = 0; i < sections.length; i++)
+    {
+        //The banner (last section) has a custom expand
+        let bannerOffset = (i == (sections.length - 1)) ? 1 : 0;
+
+        if (i < nearestSection)
+            offsets[i] = Lerp(offsets[i], 0.0 + bannerOffset, deltaTime * speed);
+        else if (i > nearestSection)
+            offsets[i] = Lerp(offsets[i], 1.0 + bannerOffset, deltaTime * speed);
+        else
+            offsets[i] = Lerp(offsets[i], -1.0 + bannerOffset, deltaTime * speed);
+
+
+        let currentTop = sectionTops[i] - offsets[i] * sectionRange;
+        let nextTop = sectionTops[i + 1] - offsets[i + 1] * sectionRange;
+
+        //Position
+        let element = sections[i];
+        element.style.top = currentTop + "px";
+
+        //Content size
+        let content = sectionContents[i];
+        let size = (currentTop - nextTop) - 30;
+        content.style.height = size + "px";
+    }
+
+
+    requestAnimationFrame(Loop);
 }
